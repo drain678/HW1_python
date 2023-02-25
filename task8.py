d1de16d35ab68c20ca5fd5b222fcac2dc60a0446
Zorina Ekaterina <ekaterina1.zorina@gmail.com>
2023-02-25 22:06:56 +0300

    comm

--- task8.py
+++ task8.py
@@ -1,43 +1,66 @@
 # вводим значения переменных
-students, varints, Petya_row, Petya_var = int(input()), int(input()), int(input()), int(input())
+students, variants, Petya_row, Petya_var = int(input()), int(input()), int(input()), int(input())
+# также добавим флаг, который будет обращаться в False,
+# если Вася не сможет получить такой же вариант как у Пети
+flag = True
+# найдем вариант Пети
 if Petya_var == 1:
-    Petya_Vanya_num_var = Petya_row * 2 - 1
-else:
-    Petya_Vanya_num_var = Petya_row * 2
-
-
-if Petya_Vanya_num_var < varints:
-    t = students - varints
-else:
-    t = (students - Petya_Vanya_num_var) // varints + varints
-    Petya_Vanya_num_var = Petya_Vanya_num_var % varints
-#u = varints - Petya_Vanya_num_var
-#t = students - varints - u
-
-if t >= Petya_Vanya_num_var:
-    if varints % 2 == 0 and varints >= Petya_Vanya_num_var:
-        Vanya_row = Petya_row + (Petya_Vanya_num_var // 2) + 1
-        Vanya_var = Petya_var
-        print(Vanya_row, Vanya_var)
-    elif (varints % 2 == 0 and Petya_Vanya_num_var % 2 != 0) or (varints % 2 == 0 and Petya_Vanya_num_var % 2 == 0):
-        Vanya_row = Petya_row + ((varints - Petya_Vanya_num_var) // 2) + 1
-        Vanya_var = Petya_var
-        print(Vanya_row, Vanya_var)
-    elif varints % 2 != 0 and Petya_Vanya_num_var % 2 != 0:
-        if Petya_var == 1:
-            Vanya_var = Petya_var + 1
-        else:
-            Vanya_var = 1
-        Vanya_row = Petya_row + ((varints - Petya_Vanya_num_var) // 2) + 1
-        print(Vanya_row, Vanya_var)
-    elif varints % 2 != 0 and Petya_Vanya_num_var % 2 == 0:
-        if Petya_var == 1:
-            Vanya_var = Petya_var + 1
-        else:
-            Vanya_var = 1
-        Vanya_row = Petya_row + ((varints - Petya_Vanya_num_var) // 2) + 1
-        print(Vanya_row, Vanya_var)
+    # если он сел справа, то нужно умножить ряд на два, т к две парты, и вычесть 1
+    Petya_Vasya_num_var2 = 0  # это будет истинный вариант
+    Petya_Vasya_num_var1 = Petya_row * 2 - 1  # а это промежуточный
+    # может получиться больше самомго большего варианта, поэтому надо разделить с остатком и будет истинный вариант
+    if Petya_Vasya_num_var1 > variants:
+        Petya_Vasya_num_var2 = Petya_Vasya_num_var1 % variants
+        if Petya_Vasya_num_var1 % variants == 0:
+            Petya_Vasya_num_var2 = variants
     else:
-        print(-1)
+        Petya_Vasya_num_var2 = Petya_Vasya_num_var1
+else:  # аналогично если он сел слева
+    Petya_Vasya_num_var2 = 0
+    Petya_Vasya_num_var1 = Petya_row * 2
+    if Petya_Vasya_num_var1 > variants:
+        Petya_Vasya_num_var2 = Petya_Vasya_num_var1 % variants
+        if Petya_Vasya_num_var1 % variants == 0:
+            Petya_Vasya_num_var2 = variants
+    else:
+        Petya_Vasya_num_var2 = Petya_Vasya_num_var1
+# теперь рассчитаем слева или справа будет сидеть Вася
+if variants % 2 == 0 and (Petya_Vasya_num_var2 % 2 == 0 or Petya_Vasya_num_var2 % 2 != 0):  # если вариантов четное количество, то он будет сидеть там же, где Петя
+    Vasya_var = Petya_var
+elif variants % 2 != 0 and (Petya_Vasya_num_var2 % 2 != 0 or Petya_Vasya_num_var2 % 2 == 0):  # иначе, они поменяются
+    if Petya_var == 1:
+        Vasya_var = Petya_var + 1
+    else:
+        Vasya_var = 1
+# если промежуточный вариант Пети и Васи не выходил за пределы основного варианта, то Вася не может сидеть перед Петей
+if Petya_Vasya_num_var1 < variants:
+    if (variants + Petya_Vasya_num_var1) % 2 == 0:  # если сумма основного варианта их варианта делится, то нужно их сложить и разделить на 2, т к парт две
+        Vasya_row = (variants + Petya_Vasya_num_var1) // 2
+    else:  # иначе, еще добавить один
+        Vasya_row = ((variants + Petya_Vasya_num_var1) // 2) + 1
+    # если количество вариантов получилось больше чем учеников, то присвоить флагу False
+    if variants + Petya_Vasya_num_var1 > students:
+        flag = False
+else:
+    # вычислим ряд Васи когда он будет сидеть после Пети
+    if ((variants + Petya_Vasya_num_var2) + (Petya_Vasya_num_var1 % variants) * variants) % 2 == 0:
+        posle_Petya = ((variants + Petya_Vasya_num_var2) + (
+                Petya_Vasya_num_var1 % variants) * variants) // 2
+    else:
+        posle_Petya = (((variants + Petya_Vasya_num_var2) + (
+                Petya_Vasya_num_var1 % variants) * variants) // 2) + 1
+    # если удвоенный вариант Пети и Васи получился меньше чем учеников, то Вася сядет за Петей
+    if Petya_Vasya_num_var1 * 2 <= students:
+        Vasya_row = posle_Petya
+    else:  # иначе, проверка на то чтобы промежуточный и истинный варианты не были одинаковыми
+        if Petya_Vasya_num_var1 != Petya_Vasya_num_var2:  # вычисление места перед Петей
+            pered_Petya = posle_Petya - (((Petya_Vasya_num_var1 - Petya_Vasya_num_var2) * 2) // 2)
+            Vasya_row = pered_Petya
+            if pered_Petya < 0:  # он если оно отрицательное, то флаг = False
+                flag = False
+        else:  # иначе, флаг = False
+            flag = False
+if flag:  # если флаг = True, то у Васи будет такой же вариант как у Пети
+    print(Vasya_row, Vasya_var)
 else:
     print(-1)
